13d912eac74a575cbab3883675018aa1a319f195
Seiya Iwabuchi <0123ook@gmail.com>
2025-07-06 11:41:58 +0900

    検索機能を追加し、書籍リストをローカルストレージから取得するように変更

--- src/controllers.js
+++ src/controllers.js
@@ -27,12 +27,56 @@ angular.module('myApp')
         $scope.goToSearch = function () {
             $location.path("/search");
         };
+        function init() {
+            const savedBookdJson = localStorage.getItem("bookList");
+            $scope.books = JSON.parse(savedBookdJson);
+        };
+        init();
     })
-    .controller('SearchController', function ($scope, $location) {
-        $scope.books = [
-            { id: 1, author: "作者", title: "タイトル" },
-            { id: 2, author: "作者", title: "タイトル" },
-            { id: 3, author: "作者", title: "タイトル" },
-        ];
-    })
-    ;
+    .controller('SearchController', ["$scope", "NdlSearchService",
+        function ($scope, NdlSearchService) {
+            $scope.books = [];
+            $scope.searchParam = {};
+            $scope.search = function () {
+                NdlSearchService.searchBooks($scope.searchParam.title, $scope.searchParam.author, 7)
+                    .then(function (response) {
+                        // DOMParserでXML文字列をパース
+                        var parser = new DOMParser();
+                        var xmlDoc = parser.parseFromString(response.data, "application/xml");
+
+                        // item要素を取得
+                        var items = xmlDoc.getElementsByTagName('item');
+                        var result = [];
+                        for (var i = 0; i < items.length; i++) {
+                            var item = items[i];
+                            // NodeListを配列に変換し、findでPIDを抽出
+                            let identifiers = Array.from(item.getElementsByTagName('dc:identifier'));
+                            let titles = Array.from(item.getElementsByTagName('title'));
+                            let authors = Array.from(item.getElementsByTagName('dc:creator'));
+                            let id = identifiers.map(elem => elem.textContent).join("-") + titles.map(elem => elem.textContent).join("-") + authors.map(elem => elem.textContent).join("-");
+                            result.push({
+                                id: id,
+                                title: item.getElementsByTagName('title')[0]?.textContent || '',
+                                author: item.getElementsByTagName('dc:creator')[0]?.textContent || '',
+                                checked: false,
+                            });
+                        }
+                        $scope.books = result;
+                    }, function () {
+                        alert('取得失敗');
+                    });
+            };
+            $scope.searchBooks = function () {
+                $scope.search();
+            };
+            $scope.addBooks = function () {
+                const selectedBooks = $scope.books.filter(book => book.checked);
+                const booksJson = localStorage.getItem("bookList");
+                let savedBooks = [];
+                if (booksJson) {
+                    books = JSON.parse(booksJson);
+                }
+                const newSaveBooks = selectedBooks.concat(savedBooks);
+                localStorage.setItem("bookList", JSON.stringify(newSaveBooks));
+            };
+        }]);
--- src/services.js
+++ src/services.js
@@ -1,6 +1,12 @@
 angular.module('myApp')
-    .service('UserService', function () {
-        this.getUser = function () {
-            // ユーザー情報取得処理
-        };
-    });
+    .service('NdlSearchService',['$http', function($http) {
+        this.searchBooks = function(title, author, count) {
+            let baseUrl = "https://ndlsearch.ndl.go.jp/api/opensearch"
+            let params = {
+                title: title,
+                creator: author,
+                cnt: count || 8,
+            };
+            return $http.get(baseUrl, { params: params });
+        }
+    }]);
